ae33d2dc0395fcf9a5f3aa1f7b3ccbf9991b25c9
G <maria.merino@es.uazuay.edu.ec>
2024-05-17 07:47:25 -0500

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -12,7 +12,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inverse <- x$getinverse()
+        if(!is.null(inverse)) {
+        	message("getting cached inverse")
+        	return(inverse)
+        }
+        data <- x$get()
+        inverse <- solve(data, ...)
+        x$setinverse(inverse)
+        inverse
 }
-git add cachematrix.R
-git commit -m "Implement makeCacheMatrix and cacheSolve functions"
-git push origin main
+
+# data <- matrix(c(1, 1, 4, 0, 3, 1, 4, 4, 0), nrow=3, ncol=3)
